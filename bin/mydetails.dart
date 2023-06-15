fa9f009401015d8a70e06d9abd8c44a56a788fa9
my pc <sakeervm4@gmail.com>
2023-06-15 16:27:54 -0700

    intial commit

---
+++ bin/mydetails.dart
@@ -0,0 +1,5 @@
+void main(){
+  print('Hafeez');
+  print(20);
+  print('rar80623@gmail.com');
+}
---
+++ bin/new.dart
@@ -0,0 +1,13 @@
+void main(){
+ String name='Hafeez';
+ int age = 20;
+ String email='rar80623@gmail.com';
+ double cgpa=9.83;
+ int a=10,b=10,c;
+ c=a+b;
+ print('my name is $name');
+ print('my age is $age');
+ print('my email is $email');
+ print('my cgpa is $cgpa');
+ print('$c');
+}
