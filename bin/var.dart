40af1dca790cac9a97a11622ee9b1ac95f203f82
my pc <sakeervm4@gmail.com>
2023-06-16 15:56:00 -0700

    intial commit

--- bin/var.dart
+++ bin/var.dart
@@ -1,3 +1,22 @@
-void main(){
+import 'dart:io';
 
+void main(){
+  print('enter name');
+  String? name=stdin.readLineSync();
+  print('enter number');
+  int age=int.parse(stdin.readLineSync()!);
+  print('enter number');
+  int ph=int.parse(stdin.readLineSync()!);
+  print('enter email');
+  String? email=stdin.readLineSync();
+  print('enter cgpa');
+  double cgpa=double.parse(stdin.readLineSync()!);
+  print('enter course');
+  String? course=stdin.readLineSync();
+  print('$name');
+  print('$age');
+  print('$ph');
+  print('$email');
+  print('$cgpa');
+  print('$course');
 }
