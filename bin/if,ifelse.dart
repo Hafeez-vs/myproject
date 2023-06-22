23098ecc64a72ccded59c02d4a5ed406aaabd082
my pc <sakeervm4@gmail.com>
2023-06-22 03:40:40 -0700

    intial commit

--- bin/if,ifelse.dart
+++ bin/if,ifelse.dart
@@ -26,23 +26,4 @@ void main() {
   }
   print('thankyou');
 
-  ///else-if ladder
-  print('please select / enter your shirt size');
-  String size = stdin.readLineSync()!;
-
-  if (size == 'xs')
-  {
-    print('your shirt size is xs');
-  }
-  else if(size == 's'){
-  print('your shirt size is s');
-  }else if(size == 'M'){
-  print('your shirt size is M');
-  }else if(size == 'L'){
-  print('your shirt size is L');
-  }else if(size == 'XL'){
-  print('your shirt size is XL');
-  }else{
-  print('your shirt size is not available');
-  }
 }
