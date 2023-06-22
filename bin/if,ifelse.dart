293d501aa39fc7e534274fefb41c852cffc65c1e
my pc <sakeervm4@gmail.com>
2023-06-22 03:12:07 -0700

    intial commit

---
+++ bin/if,ifelse.dart
@@ -0,0 +1,29 @@
+void main() {
+  print('hi user');
+  int age = 16;
+  if (age >= 18) {
+    print('welcome to vote');
+  }
+  else {
+    print('not valid');
+  }
+  print('tnk u');
+  String username='admin';
+  String password='abcdef';
+  int otp=1234;
+
+  if(username=='admin' && password=="abcdef"){
+print('email authentification success');
+
+if(otp==1234){
+print('otp verification success');
+}else {
+print('otp verification failed');
+
+}
+}else{
+    print('email authentification failed');
+  }
+    print('thankyou');
+
+}
